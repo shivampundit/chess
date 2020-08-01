78deda5e14b97539f442bbb478eae210035f4294
Shivam <shivatiwari11648@gmail.com>
2020-08-01 23:01:50 +0530

    final commit

--- app.js
+++ app.js
@@ -8,66 +8,127 @@ var queen = {
 };
 
 var board = [
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
+  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
 ];
 
 // decleration
-var steps,
-  direction,
-  flag = "yes";
-
-// multiple input till user entered no
-while (flag != "no") {
-  var flag = "yes";
-  var direction = window.prompt("Enter the direction");
-  var steps = window.prompt("Enter the number of steps");
-
-  // conversion of string in number
-  steps = parseInt(steps);
-  movequeen(direction, steps);
-
-  //continue or not
-  flag = window.prompt("do you want to continue yes/no");
-}
+var steps, direction;
 
 // funtion to move the queen object
 function movequeen(direction, steps) {
+  let temp;
+  let temp1;
+  console.log("direction " + direction);
+  console.log("steps " + steps);
+
   switch (direction) {
     case "S":
-      queen.position.row = queen.position.row + steps;
+      temp = queen.position.row + steps;
+      if (temp > 8 || temp < 0) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.row = temp;
+      }
       break;
     case "N":
-      queen.position.row = queen.position.row - steps;
+      temp = queen.position.row - steps;
+      if (temp > 8 || temp < 0) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.row = temp;
+      }
       break;
     case "E":
-      queen.position.column = queen.position.column + steps;
+      temp = queen.position.column + steps;
+      if (temp > 8 || temp < 0) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.column = temp;
+      }
       break;
     case "W":
-      queen.position.column = queen.position.column - steps;
+      temp = queen.position.column - steps;
+      if (temp > 8 || temp < 0) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.column = temp;
+      }
       break;
     case "NE":
-      queen.position.row = queen.position.row - steps;
-      queen.position.column = queen.position.column + steps;
+      temp = queen.position.row - steps;
+
+      temp1 = queen.position.column + steps;
+      if ((temp > 8 || temp < 0) && (temp1 > 8 || temp1 < 0)) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.row = temp;
+        queen.position.column = temp1;
+      }
       break;
     case "NW":
-      queen.position.row = queen.position.row - steps;
-      queen.position.column = queen.position.column - steps;
+      temp = queen.position.row - steps;
+
+      temp1 = queen.position.column - steps;
+      if ((temp > 8 || temp < 0) && (temp1 > 8 || temp1 < 0)) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.row = temp;
+        queen.position.column = temp1;
+      }
       break;
     case "SE":
-      queen.position.row = queen.position.row + steps;
-      queen.position.column = queen.position.column + steps;
+      temp = queen.position.row + steps;
+
+      temp1 = queen.position.column + steps;
+      if ((temp > 8 || temp < 0) && (temp1 > 8 || temp1 < 0)) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.row = temp;
+        queen.position.column = temp1;
+      }
       break;
     case "SW":
-      queen.position.row = queen.position.row - steps;
-      queen.position.column = queen.position.column - steps;
+      temp1 = queen.position.row - steps;
+
+      temp = queen.position.column - steps;
+      if ((temp > 8 || temp < 0) && (temp1 > 8 || temp1 < 0)) {
+        console.log("Out of boundary");
+        document.write("out of boundary");
+      } else {
+        queen.position.row = temp;
+        queen.position.column = temp1;
+      }
       break;
+
+    default:
+      document.write("Invalid Input");
   }
-  console.log(queen.position);
+}
+
+function play() {
+  var value = document.getElementById("position").value;
+  var input = value.split(" ");
+  input.forEach((num) => {
+    let number = Number(num.slice(num.length - 1));
+    let direction = num.slice(0, num.length - 1);
+    movequeen(direction, number);
+  });
+  console.log(
+    "Queen's current position ->" +
+      board[queen.position.row][queen.position.column]
+  );
 }
